6bbce958725afccfe8a08de50315a08e756880ab
Ryan Constantino <ryan.constantino93@gmail.com>
2017-10-23 03:27:06 -0700

    Restructured project away from IntelliJ structure.

--- source/app.d
+++ source/app.d
@@ -1,108 +1,127 @@
 import std.stdio;
 import std.string;
-import std.array;
-import std.socket;
 import std.process;
-import std.file;
+import std.array;
 import std.algorithm;
+import std.socket;
+import std.json;
+import std.conv;
 
-enum int[string] config = [
-        "bg_1" : 255, "bg_2" : 0, "bg_3" : 0, "bg_home" : 27, "fg_1" : 246, "fg_2"
-        : 81, "fg_3" : 0, "fg_home" : 254
-    ];
+enum string foo = import("config.json");
+enum config = parseJSON(foo);
 
-string colorSequence(int fg, int bg)
+struct Segment
+{
+    string text;
+    string colorControl;
+    string point;
+    long fg;
+    long bg;
+    string toString()
+    {
+        if (text.length > 0)
+        {
+            return colorSequence(fg, bg) ~ ` %s `.format(text) ~ point;
+        }
+        else
+        {
+            return ``;
+        }
+    }
+
+}
+
+/// Returns ANSI sequence that resets color on
+/// subsequent characters.
+string reset()
+{
+    return `\[\e[m\]`;
+}
+
+string colorSequence(long fg, long bg)
 {
     return `\[\033[38;5;%s;48;5;%sm\]`.format(fg, bg);
 }
 
+
+
 immutable string separator = "";
-immutable string dirSeparator= "";
+immutable string thinSeparator = "";
+
+Segment segment(string function(out Segment s) action)()
+{
+    Segment s;
+    s.text = action(s);
+    return s;
+}
 
 void main(string[] args)
 {
-
     auto segments = [
-        new Segment((Segment s) {
-            s.fg = config["fg_home"];
-            s.bg = config["bg_home"];
-            return ` %s `.format(executeShell(`date "+%d %b %r"`).output.strip); 
-        }),
-        new Segment((Segment s) {
-            s.fg = config["fg_1"];
-            s.bg = config["bg_1"];
-            return ` %s `.format(`\u`);
-        }),
-        new Segment((Segment s) {
-            s.fg = config["fg_2"];
-            s.bg = config["bg_2"];
-            return ` %s `.format(Socket.hostName);
-        }),
-        new Segment((Segment s) {
+        segment!((out Segment s) { 
+            s.fg = config["fg_1"].integer;
+            s.bg = config["bg_1"].integer;
+            return `\u`;
+        }), segment!((out Segment s) {
+            s.fg = config["fg_2"].integer;
+            s.bg = config["bg_2"].integer;
+            return Socket.hostName;
+        }), segment!((out Segment s){
+            s.fg = config["fg_home"].integer;
+            s.bg = config["bg_home"].integer;
+            auto pwd = executeShell("pwd");
+            if (pwd.status == 0) {
+                if (pwd.output.startsWith("/home/")) {
+                    return `~`;
+                } else {
+                    return ``;
+                }
+            } else {
+                return ` ERROR `;
+            }
+        }),segment!((out Segment s) {
             // path
-            s.fg = config["fg_1"];
-            s.bg = config["bg_1"];
+            s.fg = config["fg_1"].integer;
+            s.bg = config["bg_1"].integer;
             auto pwd = executeShell("pwd");
             string result;
             if (pwd.status == 0) {
-                result = pwd.output
-                            .strip()
-                            .split(`/`)[3..$]
-                            .map!(str => ` %s `.format(str))
-                            .join(dirSeparator);
-                if ( result.length < 1) {
-                    result = "";
+                auto dirText = pwd.output;
+                if (dirText.startsWith("/home/")) {
+                    result = dirText
+                                .strip()
+                                .split(`/`)[3..$]
+                                .join(` %s `.format(thinSeparator));
+                } else {
+                    result = dirText
+                                .strip()
+                                .split(`/`)
+                                .filter!(s => s.length > 0)
+                                .join(` %s `.format(thinSeparator));
                 }
             } else {
-                `Something went wrong (%s)`.format(pwd.output).writeln;
                 result = `error`;
             }
-
             return result;
-        })
+        }), 
+        // segment!((out Segment s) {
+        //     s.fg = config["fg_2"].integer;
+        //     s.bg = config["bg_2"].integer;
+        //     return `git`;
+        // })
     ];
-    segments = filter!(s => s.show)(segments).array();
-    // auto git = new Segment(() {
-    //     return `git`;
-    // });
-    string terminator = `\[\e[m\]` ~ `\[\033[38;5;%sm\]%s`.format(config["bg_1"],
-            separator) ~ `\[\e[m\]`;
+
+    segments = filter!(s => s.text.length > 0)(segments).array();
 
     string result;
-    foreach (int i, Segment s; segments) {
-        if (i == segments.length - 1) {
-            s.point = `\[\e[m\]` ~ `\[\033[38;5;%sm\]%s`.format(s.bg, separator) ~ `\[\e[m\]`;
-        } else {
-            s.point = colorSequence(segments[i].bg, segments[i+1].bg) ~ separator;
-        }
+
+    for (int i = 0; i < segments.length - 1; i++) {
+        segments[i].point = colorSequence(segments[i].bg, segments[i+1].bg) ~ separator;
         result ~= segments[i].toString();
     }
-    result ~= " ";
-    // result ~= terminator;
+
+    result ~= segments[$-1].toString();
+    result ~= reset ~ `\[\033[38;5;%sm\]%s`.format(segments[$-1].bg, separator) ~ reset ~ ` `;
 
     result.writeln();
-    //`\[\e[31;40m\]\u@\h>\[\e[m\]`.writeln();
-}
-
-class Segment
-{
-
-    public string segmentText;
-    public string point;
-    public int bg;
-    public int fg;
-    public bool show = true;
-
-    this(string function(Segment) action)
-    {
-        segmentText = action(this);
-        if (segmentText.length < 1) {
-            show = false;
-        }
-    }
-
-    override string toString()
-    {
-        return colorSequence(fg, bg) ~ segmentText ~ point;
-    }
 }
