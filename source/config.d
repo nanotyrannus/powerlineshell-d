48449ab67a8b34889b1393066cfd613be75a78ac
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2020-01-27 04:25:42 -0800

    Indicate if local is ahead or behind.

--- source/config.d
+++ source/config.d
@@ -5,6 +5,8 @@ enum int fg_2 = 81, bg_2 = 0;
 enum string separator = "", thin_separator = "";
 
 enum git_fg_master = 253, git_bg_master = 35;
+enum git_behind = "⬇";
+enum git_ahead = "⬆";
 enum home_fg = 251, home_bg = 33;
 enum term_fg = 253, term_bg = 237;
 enum conda_fg = 255, conda_bg = 76;
--- source/git.d
+++ source/git.d
@@ -16,24 +16,21 @@ Segment gitSegment()
             return ``;
         }
         auto status = describe.output.splitLines;
-        // auto branchRegex = ctRegex!(r"^## (?P<local>\S+?)\s?(\.{3}(?P<remote>\S+?)|(?P<detached>\(.+\))( \[(ahead (?P<ahead>\d+)(, )?)?(behind (?P<behind>\d+))?\])?)?$");
+
         // Debugging regex https://regex101.com/r/c204WA/1/tests
         auto branchRegex = ctRegex!(r"^## (?P<local>[\S]+)((\.{3}(?P<remote>\S+)|\s(?P<detached>\([\S ]+\)))( (\[(?P<ahead>ahead\s\d+)?(, )?(?P<behind>behind\s\d+)?\]))?)?$");
         auto binfo = status[0].matchFirst(branchRegex);
 
-        // // "local: %s, remote: %s, ahead: %s, behind: %s".format(binfo["local"], binfo["remote"], binfo["ahead"], binfo["behind"]).writeln();
         string result;
         if (binfo["behind"] != null)
         {
-            // s.children ~= segment!((out Segment s) {
-            //     string text = "%s %s".format(binfo["behind"], config["segments"]["git"]["behind"].str);
-            //     return "";
-            // });
+            result ~= git_behind;
         }
         else if (binfo["ahead"] != null)
         {
+            result ~= git_ahead;
             // s.children ~= segment!((out Segment s){
-            //     return "%s %s".format(binfo["ahead"], config["segments"]["git"]["behind"].str);
+            //     return "%s %s".format(binfo["ahead"], git_ahead);
             // });
         }
         s.fg = git_fg_master;
@@ -43,7 +40,7 @@ Segment gitSegment()
         }
         if (binfo["local"] != null)
         {
-            result ~= binfo["local"];
+            result = binfo["local"] ~ " " ~ result;
         }
         return result;
     });
